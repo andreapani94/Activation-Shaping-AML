21da185d0b10f8cadd26dba5eff86786cccebb73
Andrea Pani <andrp94@gmail.com>
2024-01-26 19:08:55 +0100

    detach added

--- models/resnet.py
+++ models/resnet.py
@@ -19,18 +19,17 @@ class DAResNet18(nn.Module):
         self.resnet = resnet18(weights=ResNet18_Weights)
         self.resnet.fc = nn.Linear(self.resnet.fc.in_features, 7)
         self.actmaps_target = []
-        self.actmap_index = 0
-
+        self.actmaps_index = 0
 
     def forward(self, x):
         return self.resnet(x)
     
     def rec_actmaps_hook(self, module, input, output):
-        self.actmaps_target.append(output)
+        self.actmaps_target.append(output.detach())
         return output
     
     def asm_source_hook(self, module, input, output):
-        mask = self.actmaps_target[self.actmap_index]
+        mask = self.actmaps_target[self.actmaps_index]
         mask_bin = (mask > 0).float()
         self.actmaps_index += 1
         output_bin = (output > 0).float()
